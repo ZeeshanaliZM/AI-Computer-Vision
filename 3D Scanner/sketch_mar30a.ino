86b532c58b7d69ecaf31f2309d3c8d980e82a884
ZeeshanaliZM <97593916+ZeeshanaliZM@users.noreply.github.com>
2023-04-12 13:13:20 +0530

    Update sketch_mar30a.ino

--- 3D Scanner/sketch_mar30a.ino
+++ 3D Scanner/sketch_mar30a.ino
@@ -1,18 +1,45 @@
-int val;
-
+import cv2
+import matplotlib.pyplot as plt
+import serial
+import time
+while(bool(input('Enter a value to begin'))==False):
+    pass
+COMPort = input('Enter the COM port value')
+countImages = 0
+ArduinoSerial = serial.Serial(COMPort,9600)
+time.sleep(2)
+capture = cv2.VideoCapture(0)
+captureFlag = False
+ArduinoSerial.write(b'1')
+print('...Starting Motor...')
+time.sleep(5)
+while(captureFlag==False):
+    pass
+    
+    
+const int dirPin = 13;
+const int stepPin = 12;
+const int stepsPerRevolution = 200;
+int startSignal;
 void setup() {
-  Serial.begin(9600);
   pinMode(LED_BUILTIN,OUTPUT);
   digitalWrite(LED_BUILTIN,LOW);
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  Serial.begin(9600);
 }
-
 void loop() {
   if (Serial.available() > 0){
-    val = Serial.read();
-    Serial.write("\n");
-    if(val=='1'){
-      digitalWrite(LED_BUILTIN,HIGH);
-      }
-    if(val=='0'){
-      digitalWrite(LED_BUILTIN,LOW);
-      }}}
+    startSignal = Serial.read();}
+  if(startSignal == '1')
+  {digitalWrite(LED_BUILTIN,HIGH);
+    rotateMotor();}
+    }
+
+void rotateMotor(){
+  digitalWrite(dirPin, HIGH);
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(2000);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(2000);  
+}
